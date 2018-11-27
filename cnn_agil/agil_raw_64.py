84e6b60074cefa6589a3529a538e2a5d75c642bd
Dawei Liang (David) <32776482+dawei-liang@users.noreply.github.com>
2018-11-27 00:48:20 -0600

    Add files via upload

---
+++ cnn_agil/agil_raw_64.py
@@ -0,0 +1,288 @@
+'''adopted from Arcade-Learning-Environment repository:
+https://github.com/corgiTrax/Arcade-Learning-Environment/blob/master/gaze/main-salient-image%2Bopf-past4.py '''
+
+import numpy as np, keras as K, sys
+import keras.layers as L
+from keras.models import Model
+import tensorflow as tf 
+import random
+import os
+from numpy.random import seed
+from tensorflow import set_random_seed
+
+import util_calculations as MU
+import load_data_for_cnn
+import base_misc_utils
+import compress_data
+import cv2
+
+'''Fix random seed'''
+#seed(0)
+#set_random_seed(0)
+
+os.environ['MKL_NUM_THREADS']='272'
+os.environ['GOTO_NUM_THREADS']='272'
+os.environ['OMP_NUM_THREADS']='272'
+os.environ['openmp']='True'
+
+
+print("Architecture: agil_bianzhong")
+
+
+BATCH_SIZE = 64
+validation_size = 200
+validation_BATCH_SIZE = 50
+num_epoch = 20
+
+resume_model = False
+predict_mode = True
+compress_channel_1=True   # Whether to stack and save img by every 2 channels first (sub 1)
+compress_channel_2 = True 
+compress_channel_3 = True   
+
+save_index = '55'   # File index to load the well-trained model and save predictions
+dropout = 0.5
+k = 3   # input img channels
+stride = 1
+SHAPE = (120,160,k) # height * width * channel This cannot read from file and needs to be provided here
+
+print('predict?', predict_mode)
+
+if not predict_mode: # If training, save in target dir
+    expr = base_misc_utils.ExprCreaterAndResumer('result_sem2', \
+						postfix="agil_dp%.1f_batch%d_chan%d_stride%d" % (dropout,BATCH_SIZE,k,stride))
+
+base_misc_utils.save_GPU_mem_keras()
+MU.keras_model_serialization_bug_fix()
+
+# Load model
+if resume_model:
+    model = expr.load_weight_and_training_config_and_state()
+
+# Write a new model
+else:
+    ###############################
+    # Architecture of the network #
+    ###############################
+    # First channel: image
+
+    x_inputs=L.Input(shape=SHAPE)    
+    x=x_inputs #inputs is used by the line "Model(inputs, ... )" below
+
+    # conv_1
+    x = L.Conv2D(32, (8,8), strides=4, activation='relu', padding='same', data_format="channels_last", name='block1_conv1')(x)
+    x = L.BatchNormalization()(x)
+    x = L.Dropout(dropout)(x)
+    print("conv_1_1", x.shape)
+    x = L.Conv2D(64, (4,4), strides=2, activation='relu', padding='same', data_format="channels_last", name='block1_conv2')(x)
+    x = L.BatchNormalization()(x)
+    x = L.Dropout(dropout)(x)
+    print("conv_1_2", x.shape)
+
+    # conv_2
+    x = L.Conv2D(64, (3,3), strides=1, activation='relu', padding='same', data_format="channels_last", name='block2_conv1')(x)
+    x = L.BatchNormalization()(x)
+    x = L.Dropout(dropout)(x)
+    print("conv_2", x.shape)
+    
+    # deconv
+    deconv1 = L.Conv2DTranspose(64, (3,3), strides=1, padding='same')
+    x = deconv1(x)
+    print("deconv_1", deconv1.output_shape)
+    x = L.Activation('relu')(x)
+    x = L.BatchNormalization()(x)
+    x = L.Dropout(dropout)(x)
+ 
+    deconv2 = L.Conv2DTranspose(32, (4,4), strides=2, padding='same')
+    x = deconv2(x)
+    print("deconv_2", deconv2.output_shape)
+    x = L.Activation('relu')(x)
+    x = L.BatchNormalization()(x)
+    x = L.Dropout(dropout)(x) 
+    deconv3 = L.Conv2DTranspose(1, (8,8), strides=4, padding='same')
+    x = deconv3(x)
+    print ("deconv_3", deconv3.output_shape)
+    x = L.Activation('relu')(x)
+    x = L.BatchNormalization()(x)
+    # Output layer
+    x_output = L.Activation(MU.my_softmax)(x)    
+
+    # Define model
+    model=Model(inputs=x_inputs, outputs=x_output)
+
+    # Load weights
+    #model.load_weights('/home1/05563/dl33629/WORK_maverick/gaze_prediction/Walking_Gaze_Modeling/cnn/result_sem2/53_agil_grey_dp0.5_batch64_chan1_stride1/weights.20-4.41.hdf5', by_name=True)
+
+    # Optimizer
+    opt=K.optimizers.Adadelta(lr=1, rho=0.95, epsilon=1e-08, decay=0.0)
+    #opt=K.optimizers.Adam(lr=0.001, beta_1=0.9, beta_2=0.999, epsilon=1e-08)
+        
+    model.compile(loss=MU.my_kld, optimizer=opt, metrics=[MU.NSS])
+
+# Load training data and labels(heatmaps) 
+
+if compress_channel_1:
+    training_data_object = load_data_for_cnn.read_data('/home1/05563/dl33629/WORK_maverick/gaze_prediction/Walking_Gaze_Modeling/frames_in_use_1/',
+                                                   '/home1/05563/dl33629/WORK_maverick/gaze_prediction/Walking_Gaze_Modeling/groundtruth_heatmap_1/',
+                                                   120, 160)
+
+    sub1_data, sub1_labels, frame_index, heatmap_index = training_data_object.load_data()
+    training_data_object2 = load_data_for_cnn.read_data('/home1/05563/dl33629/WORK_maverick/gaze_prediction/Walking_Gaze_Modeling/frames_in_use_1_flip/',
+                                                   '/home1/05563/dl33629/WORK_maverick/gaze_prediction/Walking_Gaze_Modeling/groundtruth_heatmap_1_flip/',
+                                                   120, 160)
+
+    sub1_data_flip, sub1_labels_flip, frame_index_flip, heatmap_index_flip = training_data_object2.load_data()
+
+    #compress_data.save(k, './dir_to_save_compressed_frames_sub1_k6/', training_data, frame_index)
+
+#training_data = compress_data.load_data(k, './dir_to_save_compressed_frames_sub1_k6/', 120, 160)
+#training_labels = compress_data.load_label('/home1/05563/dl33629/WORK_maverick/gaze_prediction/Walking_Gaze_Modeling/groundtruth_heatmap_1/',
+ #                                                  120, 160)
+print('sub1 data loaded, shape:', sub1_data.shape, sub1_labels.shape)
+
+# Load test data and labels(heatmaps) 
+if compress_channel_2:
+    val_data_object = load_data_for_cnn.read_data('/home1/05563/dl33629/WORK_maverick/gaze_prediction/Walking_Gaze_Modeling/frames_in_use_2/',
+                                                   '/home1/05563/dl33629/WORK_maverick/gaze_prediction/Walking_Gaze_Modeling/groundtruth_heatmap_2/',
+                                                       120, 160) 
+    sub2_data, sub2_labels, frame_index_test, heatmap_index_test = val_data_object.load_data_validation()
+    val_data_object2 = load_data_for_cnn.read_data('/home1/05563/dl33629/WORK_maverick/gaze_prediction/Walking_Gaze_Modeling/frames_in_use_2_flip/',
+                                                   '/home1/05563/dl33629/WORK_maverick/gaze_prediction/Walking_Gaze_Modeling/groundtruth_heatmap_2_flip/',
+                                                   120, 160)
+
+    sub2_data_flip, sub2_labels_flip, frame_index_test_flip, heatmap_index_test_flip = val_data_object2.load_data_validation()
+
+
+#    compress_data.save(k, './dir_to_save_compressed_frames_sub2_k6/', test_data, frame_index_test)
+
+#test_data = compress_data.load_data(k, './dir_to_save_compressed_frames_sub2_k6/', 120, 160)
+#test_labels = compress_data.load_label('/home1/05563/dl33629/WORK_maverick/gaze_prediction/Walking_Gaze_Modeling/groundtruth_heatmap_2/',
+ #                                                  120, 160)
+print('sub2 data loaded, shape:', sub2_data.shape, sub2_labels.shape)
+
+
+# Load sub3 data and labels(heatmaps) 
+if compress_channel_3:
+    test_data_object = load_data_for_cnn.read_data('/home1/05563/dl33629/WORK_maverick/gaze_prediction/Walking_Gaze_Modeling/frames_in_use_3/',
+                                                   '/home1/05563/dl33629/WORK_maverick/gaze_prediction/Walking_Gaze_Modeling/groundtruth_heatmap_3/',
+                                                       120, 160) 
+    sub3_data, sub3_labels, frame_index_val, heatmap_index_val = test_data_object.load_data_test()
+    test_data_object2 = load_data_for_cnn.read_data('/home1/05563/dl33629/WORK_maverick/gaze_prediction/Walking_Gaze_Modeling/frames_in_use_3_flip/',
+                                                   '/home1/05563/dl33629/WORK_maverick/gaze_prediction/Walking_Gaze_Modeling/groundtruth_heatmap_3_flip/',
+                                                   120, 160)
+
+    sub3_data_flip, sub3_labels_flip, frame_index_val_flip, heatmap_index_val_flip = test_data_object2.load_data_test()
+
+#    compress_data.save(k, './dir_to_save_compressed_frames_sub3_k12/', test_data, frame_index_test)
+
+#val_data = compress_data.load_data(k, './dir_to_save_compressed_frames_sub3_k12/', 120, 160)
+#val_labels = compress_data.load_label('/home1/05563/dl33629/WORK_maverick/gaze_prediction/Walking_Gaze_Modeling/groundtruth_heatmap_3/',
+#                                                   120, 160)
+print('sub3 data loaded, shape:', sub3_data.shape, sub3_labels.shape)
+
+# Combine subject data and labels
+training_data, training_labels = np.vstack((sub1_data[0:3300], sub2_data[0:3800])), np.vstack((sub1_labels[0:3300], sub2_labels[0:3800]))
+training_data, training_labels = np.vstack((training_data, sub3_data[0:3500])), np.vstack((training_labels, sub3_labels[0:3500]))
+
+training_data, training_labels = np.vstack((training_data, sub1_data_flip[0:3300])), np.vstack((training_labels, sub1_labels_flip[0:3300]))
+training_data, training_labels = np.vstack((training_data, sub2_data_flip[0:3800])), np.vstack((training_labels, sub2_labels_flip[0:3800]))
+training_data, training_labels = np.vstack((training_data, sub3_data_flip[0:3500])), np.vstack((training_labels, sub3_labels_flip[0:3500]))
+print('training set size:', training_data.shape, training_labels.shape)
+
+test_data, test_labels = np.vstack((sub1_data[3300:], sub2_data[3800:])), np.vstack((sub1_labels[3300:], sub2_labels[3800:]))
+test_data, test_labels = np.vstack((test_data, sub3_data[3500:])), np.vstack((test_labels, sub3_labels[3500:]))
+
+print('test set size:', test_data.shape, test_labels.shape)
+
+#std
+mean = np.mean(training_data)
+std = np.std(training_data)
+training_data = (training_data - mean) / std
+print('training mean&std:', mean, std)
+test_data = (test_data - mean) / std
+
+if not predict_mode: 
+
+    # if training mode, save model and source codes
+    expr.dump_src_code_and_model_def('/home1/05563/dl33629/WORK_maverick/gaze_prediction/Walking_Gaze_Modeling/cnn/vgg_dec.py', model)
+    
+    #training_data_object.load_data(BATCH_SIZE = BATCH_SIZE, 
+    #                               validation_size=validation_size,
+    #                               validation_BATCH_SIZE=validation_BATCH_SIZE)
+    # 
+    model.fit(training_data, training_labels, BATCH_SIZE, epochs=num_epoch,
+        validation_data=(test_data, test_labels),
+        shuffle=True, verbose=2,
+        callbacks=[K.callbacks.TensorBoard(log_dir=expr.dir),
+            K.callbacks.ReduceLROnPlateau(monitor='val_loss', factor=0.5, patience=num_epoch, min_lr = 0.00001),
+            K.callbacks.ModelCheckpoint(expr.dir+"/weights.{epoch:02d}-{val_loss:.2f}.hdf5", monitor='val_loss', verbose=0, save_best_only=False, save_weights_only=False, mode='auto', period=1),
+            base_misc_utils.PrintLrCallback()])
+
+   
+    #model.fit_generator(training_data_object.batch_input(mode='training'), 
+    #        steps_per_epoch=15,
+    #        epochs=num_epoch,
+    #        verbose=2,
+    #        callbacks=[K.callbacks.TensorBoard(log_dir=expr.dir),
+    #                   K.callbacks.ReduceLROnPlateau(monitor='val_loss', factor=0.5, patience=num_epoch, min_lr = 0.00001),
+    #                   base_misc_utils.PrintLrCallback()], use_multiprocessing=True, shuffle=True)
+            #validation_data = training_data_object.batch_input(mode='validation'),
+            #validation_steps = 3)
+
+    expr.save_weight_and_training_config_state(model)
+    expr.printdebug('Successfully saved')
+    expr.logfile.close()
+
+# if prediction mode
+elif predict_mode:
+    rootdir = './result_sem2/' + str(save_index) + '_agil_dp0.5_batch'+ str(BATCH_SIZE) + '_chan'+ str(k) + '_stride1/'
+    model.load_weights(rootdir +'weights.18-3.82.hdf5')
+
+    #print ("Evaluating model...")
+    #score = model.evaluate(test_data, test_labels, BATCH_SIZE, 0)
+    #print ("Test score is: " , score)
+
+    '''
+    print ("Predicting training results...")
+    if not os.path.exists(rootdir + 'prediction/sub1/'):
+        os.makedirs(rootdir + 'prediction/sub1/')
+    pred_training = model.predict(training_data, BATCH_SIZE)
+    training_data = 0
+    for i in range(pred_training.shape[0]):
+        np.savez(rootdir + 'prediction/sub1/' + 'prediction%d' %i, heatmap=pred_training[i])
+
+    print ("Predicted and saved(training).")'''
+
+    print ("Copying truth frames...")
+
+    training_data = 0
+    if not os.path.exists(rootdir + 'prediction/sub1/'):   # File for prediction
+        os.makedirs(rootdir + 'prediction/sub1/')
+    if not os.path.exists(rootdir + 'prediction/groundtruth_frames_sub1/'):   # File for raw groundtruth frames
+        os.makedirs(rootdir + 'prediction/groundtruth_frames_sub1/')
+
+    #truth_dir = '/home1/05563/dl33629/WORK_maverick/gaze_prediction/Walking_Gaze_Modeling/frames_groundtruth_1/'
+    truth_dir = '/home1/05563/dl33629/WORK_maverick/gaze_prediction/Walking_Gaze_Modeling/frames_in_use_1/'
+    truth_sets = [x for x in os.listdir(truth_dir) if x.endswith('.jpg')]
+    # Sort file name by frame index
+    for i in range(len(truth_sets)):
+        truth_sets[i] = int(truth_sets[i].strip('frame').strip('.jpg'))
+    truth_sets.sort()
+    for i in range(len(truth_sets)):
+         truth_sets[i] = 'frame' + str(truth_sets[i]) + '.jpg'
+
+    # Save truth frames
+    for i in range(len(truth_sets)):
+          if i >= 0:
+              frame_idx = int(truth_sets[i].strip('frame').strip('.jpg'))
+              img = cv2.imread(truth_dir + truth_sets[i])
+              cv2.imwrite(rootdir + 'prediction/groundtruth_frames_sub1/' + 'frame%s.jpg' % frame_idx, img)
+
+    # Predict
+    print ("Predicting test results...")
+    pred = model.predict(sub1_data, BATCH_SIZE)   
+    test_data = 0
+    for i in range(pred.shape[0]):
+        np.savez(rootdir + 'prediction/sub1/' + 'prediction%d' %i, heatmap=pred[i])
+
+    print ("Predicted and saved(test).") 
--- cnn_agil/load_data_for_cnn.py
+++ cnn_agil/load_data_for_cnn.py
@@ -8,7 +8,7 @@ import os
 import cv2
 import numpy as np
 import heatmap_generation
-
+import copy as cp
 #%%
 
 class read_data:
@@ -21,33 +21,159 @@ class read_data:
     def load_data(self):   
         # Load frames as training data
         frame_sets = [x for x in os.listdir(self.dir_to_save_frames_in_use) if x.endswith('.jpg')]
-        training_data = np.empty((100, self.height, self.width, 3))
-        count = 0
+        # Sort file name by frame index
         for i in range(len(frame_sets)):
-            # Load 100 frames
-            if i < 100:
-                img = cv2.imread(self.dir_to_save_frames_in_use + frame_sets[i])   
-                training_data[count] = img
-                count += 1
+             frame_sets[i] = int(frame_sets[i].strip('frame').strip('.jpg'))
+        frame_sets.sort()
+        for i in range(len(frame_sets)):
+             frame_sets[i] = 'frame' + str(frame_sets[i]) + '.jpg'
+
+        training_data = np.empty((4900, self.height, self.width, 3))
+        count = 0
+        frame_idx_list = []
+        for i in range(len(frame_sets)):
+             if i < 4900:
+                 frame_idx = int(frame_sets[i].strip('frame').strip('.jpg'))
+                 frame_idx_list.append(frame_idx)
+                 print('frame:', frame_idx)   # For index check
+                 img = cv2.imread(self.dir_to_save_frames_in_use + frame_sets[i])
+                 training_data[count] = cp.deepcopy(img)
+                 #training_data[count] = (training_data[count] - np.mean(training_data[count])) / np.std(training_data[count])
+                 count += 1
                 
         # Load ground truth heatmaps as labels
-        truth_heatmap_sets = [x for x in os.listdir(self.dir_ground_truth_heatmap) if x.endswith('.npz')]  
-        training_labels = np.empty((100, self.height, self.width))
-        heatmap_object = heatmap_generation.heatmap(1024, 576)
-        count = 0
+        truth_heatmap_sets = [x for x in os.listdir(self.dir_ground_truth_heatmap) if x.endswith('.npz')]
+        # Sort file name by frame index
         for j in range(len(truth_heatmap_sets)):
-            # Load 100 labels
-            if j < 100:
-                img = np.load(self.dir_ground_truth_heatmap + truth_heatmap_sets[j])['heatmap']   
-                img = heatmap_object.normalize(img)   # heatmap normalization
-                training_labels[count] = img
+             truth_heatmap_sets[j] = int(truth_heatmap_sets[j].strip('heatmap').strip('.npz'))
+        truth_heatmap_sets.sort()
+        for j in range(len(truth_heatmap_sets)):
+             truth_heatmap_sets[j] = 'heatmap' + str(truth_heatmap_sets[j]) + '.npz'
+
+  
+        training_labels = np.empty((4900, self.height, self.width))
+        heatmap_object = heatmap_generation.heatmap(self.width, self.height)
+        count = 0
+        heatmap_idx_list = []
+        for j in range(len(truth_heatmap_sets)):
+             if j < 4900:
+                heatmap_idx = int(truth_heatmap_sets[j].strip('heatmap').strip('.npz'))
+                heatmap_idx_list.append(heatmap_idx)
+                print('heatmap:', heatmap_idx)   # For index check
+                img2 = np.load(self.dir_ground_truth_heatmap + truth_heatmap_sets[j])['heatmap']   
+                img2 = heatmap_object.normalize(img2)   # heatmap normalization
+                training_labels[count] = cp.deepcopy(img2)
                 count += 1
                 
         # Reshape input labels (heatmaps)
-        training_labels = np.reshape(training_labels, (training_labels.shape[0], 576, 1024, 1))
-        # Whitening
-        training_data = (training_data - np.mean(training_data)) / np.std(training_data)
+        training_labels = np.reshape(training_labels, (training_labels.shape[0], self.height, self.width, 1))
         
-        return training_data, training_labels
+        return training_data, training_labels, frame_idx_list, heatmap_idx_list
+    
+
+    def load_data_validation(self):   
+        # Load frames as validation data
+        frame_sets = [x for x in os.listdir(self.dir_to_save_frames_in_use) if x.endswith('.jpg')]
+        # Sort file name by frame index
+        for i in range(len(frame_sets)):
+             frame_sets[i] = int(frame_sets[i].strip('frame').strip('.jpg'))
+        frame_sets.sort()
+        for i in range(len(frame_sets)):
+             frame_sets[i] = 'frame' + str(frame_sets[i]) + '.jpg'
+
+        validation_data = np.empty((5700, self.height, self.width, 3))
+        count = 0
+        frame_idx_list = []
+        for i in range(len(frame_sets)):
+             if i < 5700:
+                 frame_idx = int(frame_sets[i].strip('frame').strip('.jpg'))
+                 frame_idx_list.append(frame_idx)
+                 print('frame:', frame_idx)   # For index check
+                 img_val = cv2.imread(self.dir_to_save_frames_in_use + frame_sets[i])                  
+                 validation_data[count] = cp.deepcopy(img_val)
+                 #validation_data[count] = (validation_data[count] - np.mean(validation_data[count])) / np.std(validation_data[count])
+                 count += 1
+                
+        # Load ground truth heatmaps as labels
+        truth_heatmap_sets = [x for x in os.listdir(self.dir_ground_truth_heatmap) if x.endswith('.npz')]
+        # Sort file name by frame index
+        for j in range(len(truth_heatmap_sets)):
+             truth_heatmap_sets[j] = int(truth_heatmap_sets[j].strip('heatmap').strip('.npz'))
+        truth_heatmap_sets.sort()
+        for j in range(len(truth_heatmap_sets)):
+             truth_heatmap_sets[j] = 'heatmap' + str(truth_heatmap_sets[j]) + '.npz'
+
+  
+        validation_labels = np.empty((5700, self.height, self.width))
+        heatmap_object = heatmap_generation.heatmap(self.width, self.height)
+        count = 0
+        heatmap_idx_list = []
+        for j in range(len(truth_heatmap_sets)):
+             if j < 5700:
+             	heatmap_idx = int(truth_heatmap_sets[j].strip('heatmap').strip('.npz'))
+                heatmap_idx_list.append(heatmap_idx)
+             	print('heatmap:', heatmap_idx)   # For index check
+             	img2_val = np.load(self.dir_ground_truth_heatmap + truth_heatmap_sets[j])['heatmap']   
+             	img2_val = heatmap_object.normalize(img2_val)   # heatmap normalization
+             	validation_labels[count] = cp.deepcopy(img2_val)
+             	count += 1
+                
+        # Reshape input labels (heatmaps)
+        validation_labels = np.reshape(validation_labels, (validation_labels.shape[0], self.height, self.width, 1))
+        
+        return validation_data, validation_labels, frame_idx_list, heatmap_idx_list
+
+
+    def load_data_test(self):   
+        # Load frames as validation data
+        frame_sets = [x for x in os.listdir(self.dir_to_save_frames_in_use) if x.endswith('.jpg')]
+        # Sort file name by frame index
+        for i in range(len(frame_sets)):
+             frame_sets[i] = int(frame_sets[i].strip('frame').strip('.jpg'))
+        frame_sets.sort()
+        for i in range(len(frame_sets)):
+             frame_sets[i] = 'frame' + str(frame_sets[i]) + '.jpg'
+
+        test_data = np.empty((5300, self.height, self.width, 3))
+        count = 0
+        frame_idx_list = []
+        for i in range(len(frame_sets)):
+             if i >= 0 and i < 5300:
+                 frame_idx = int(frame_sets[i].strip('frame').strip('.jpg'))
+                 frame_idx_list.append(frame_idx)
+                 print('frame:', frame_idx)   # For index check
+                 img_test = cv2.imread(self.dir_to_save_frames_in_use + frame_sets[i])
+                 test_data[count] = cp.deepcopy(img_test)
+                 #test_data[count] = (test_data[count] - np.mean(test_data[count])) / np.std(test_data[count])
+                 count += 1
+                
+        # Load ground truth heatmaps as labels
+        truth_heatmap_sets = [x for x in os.listdir(self.dir_ground_truth_heatmap) if x.endswith('.npz')]
+        # Sort file name by frame index
+        for j in range(len(truth_heatmap_sets)):
+             truth_heatmap_sets[j] = int(truth_heatmap_sets[j].strip('heatmap').strip('.npz'))
+        truth_heatmap_sets.sort()
+        for j in range(len(truth_heatmap_sets)):
+             truth_heatmap_sets[j] = 'heatmap' + str(truth_heatmap_sets[j]) + '.npz'
+
+  
+        test_labels = np.empty((5300, self.height, self.width))
+        heatmap_object = heatmap_generation.heatmap(self.width, self.height)
+        count = 0
+        heatmap_idx_list = []
+        for j in range(len(truth_heatmap_sets)):
+             if j >= 0 and j < 5300:
+             	heatmap_idx = int(truth_heatmap_sets[j].strip('heatmap').strip('.npz'))
+                heatmap_idx_list.append(heatmap_idx)
+             	print('heatmap:', heatmap_idx)   # For index check
+             	img3_test = np.load(self.dir_ground_truth_heatmap + truth_heatmap_sets[j])['heatmap']   
+             	img3_test = heatmap_object.normalize(img3_test)   # heatmap normalization
+             	test_labels[count] = cp.deepcopy(img3_test)
+             	count += 1
+                
+        # Reshape input labels (heatmaps)
+        test_labels = np.reshape(test_labels, (test_labels.shape[0], self.height, self.width, 1))
+        
+        return test_data, test_labels, frame_idx_list, heatmap_idx_list
     
 
