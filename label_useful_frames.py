84e202d82cb534c120a3603b73a781509cdfbf73
Dawei Liang (David) <32776482+dawei-liang@users.noreply.github.com>
2019-05-28 22:17:09 -0500

    Add files via upload

--- label_useful_frames.py
+++ label_useful_frames.py
@@ -12,26 +12,25 @@ import check_dirs
 import scipy.io as sio
 import numpy as np
 
-matfn = './sub1.mat'
+import read_frameInfo
+
+matfn = config.groundtruth_file
 data=sio.loadmat(matfn)
 fix_index = data['fixation_frames']
 fix_index = fix_index.astype(np.int32)
+subject = 1   # Can be 1,2,3
 
-check_dirs.check_dir(config.dir_to_save_frames_in_use)
+check_dirs.check_dir(config.dir_to_save_frames_in_use)    
 frame_sets = [x for x in os.listdir(config.dir_to_load_frames) if x.endswith('.jpg')]   # Load frames
 print('Number of frames:', len(frame_sets))
-    
+
+# Import frame idx
+subFrames, _, _ = read_frameInfo.frameInfo('./frameInfo.mat', subject=subject)   
 
 for frame in frame_sets:
     index = int(frame.strip('frame').strip('.jpg'))   # Get loaded frame index
     # Range of frames in use
-    if ((index >= 4240 and index < 5308) or \
-        (index >= 5332 and index < 6363) or \
-        (index >= 6701 and index < 7778) or \
-        (index >= 7814 and index < 8917) or \
-        (index >= 9329 and index < 11444) or \
-        (index >= 11678 and index < 13857)) and \
-        fix_index[index] == 1:
+    if index in subFrames and fix_index[index] == 1:
         shutil.copy(config.dir_to_load_frames + frame, config.dir_to_save_frames_in_use) 
         try:
             os.rename(config.dir_to_save_frames_in_use + frame, 
